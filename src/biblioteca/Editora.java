b9ec243e289e93099f127df3a1e78be9030cc5c8
rcastelar <30738176+rcastelar@users.noreply.github.com>
2017-08-23 12:04:01 -0300

    Add files via upload

--- src/biblioteca/Editora.java
+++ src/biblioteca/Editora.java
@@ -10,5 +10,22 @@ package biblioteca;
  * @author f32cpd02
  */
 public class Editora {
-    
+      private int id;
+    private String nome;
+    public Editora(int bid, String bnome){
+        this.id=bid;
+        this.nome=bnome;
+}
+    public int getId(){
+        return this.id;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setId(int bid){
+        this.id=bid;
+    }
+    public void setNome(String bnome){
+        this.nome=bnome;
+    }
 }
