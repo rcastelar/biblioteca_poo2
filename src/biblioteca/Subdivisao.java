5c1acd51da0ebfd97ec96193836dfaca5c1ee087
rcastelar <30738176+rcastelar@users.noreply.github.com>
2017-08-23 15:08:28 -0300

    Add files via upload

--- src/biblioteca/Subdivisao.java
+++ src/biblioteca/Subdivisao.java
@@ -9,6 +9,34 @@ package biblioteca;
  *
  * @author f32cpd02
  */
-public class Subdivisao extends Local{
-    private int posicao;
+public class Subdivisao {
+    private int id;
+    private int local_id;
+    private String descricao;
+      public Subdivisao(int bid, int blocal_id, String bdescricao){
+        this.id=bid;
+        this.local_id=blocal_id;
+        this.descricao= bdescricao;
+    }
+    public Subdivisao(){
+    }
+        public int getId(){
+        return this.id;
+    }
+    public int getLocal_id(){
+        return this.local_id;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public void setId(int bid){
+        this.id=bid;
+    }
+    public void setLocal_id(int blocal_id){
+        this.local_id=blocal_id;
+    }
+    public void setDescricao(String bdescricao){
+        this.descricao=bdescricao;
+    }
 }
