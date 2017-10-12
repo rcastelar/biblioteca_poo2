65b858c60d068ab3cae32c8866c4141edfebebc5
MarianaQuintiliano <m183711@g.unicamp.br>
2017-10-12 12:28:41 -0300

    Exemplar_DAO implementada

--- src/biblioteca/Exemplar_DAO.java
+++ src/biblioteca/Exemplar_DAO.java
@@ -5,9 +5,54 @@
  */
 package biblioteca;
 
+import bd.DBHandler;
+import java.sql.*;
+import java.util.*;
+
 /**
  * @author f32cpd01
  */
 public class Exemplar_DAO {
+
+    Exemplar_DAO(){
+
+    }
+
+    public ResultSet GetAllExemplar() throws SQLException, ClassNotFoundException {
+        DBHandler Exemplares = new DBHandler();
+        return Exemplares.querry("SELECT * FROM exemplar");
+    }
+
+    public ResultSet GetExemplarCampos(List<String> campo, List<String> valor) throws SQLException, ClassNotFoundException {
+        DBHandler Exemplares = new DBHandler();
+        if(!campo.isEmpty() && !valor.isEmpty())
+        {
+            String mysql = new String();
+            mysql = "SELECT * FROM exemplar WHERE "+campo.get(0)+"='"+valor.get(1)+"'";
+
+            for(int i=0; i<campo.size(); i++)
+            {
+                if(i==0)
+                    continue;
+                else
+                    mysql.concat(" AND "+campo.get(i)+"='"+valor.get(i)+"'");
+            }
+
+            return Exemplares.querry(mysql);
+        }
+        else
+            return null;
+    }
+
+    public ResultSet GetExemplarInList(String campo, List<String> valor) throws SQLException, ClassNotFoundException {
+        DBHandler Exemplares = new DBHandler();
+        if(!valor.isEmpty())
+        {
+            return Exemplares.querry("SELECT * FROM exemplar WHERE "+campo+"in:valor");
+            //Precisamos testar para ver se será possivel fazer essa in: valor
+        }
+        else
+            return null;
+    }
 
 }
--- src/biblioteca/Livro_DAO.java
+++ src/biblioteca/Livro_DAO.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import bd.DBHandler;
+
 package biblioteca;
+
 import java.sql.SQLException;
 
 /**
