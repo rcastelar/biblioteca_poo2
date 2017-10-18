6265625406404fab6fbbb3508e2c1c66d13e5b9a
rcastelar <raniericastel@gmail.com>
2017-10-18 14:49:19 -0200

    Starting to build book edition.

--- src/Views/ViewEditLivros.java
+++ src/Views/ViewEditLivros.java
@@ -2,24 +2,14 @@ package Views;
 
 import biblioteca.Controller;
 import biblioteca.Livro;
-import biblioteca.Livro_DAO;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+
 
 import java.net.URL;
-import java.sql.Array;
-import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
+
 import java.util.ResourceBundle;
 
 public class ViewEditLivros implements Initializable, ControlledScreen{
@@ -28,20 +18,17 @@ public class ViewEditLivros implements Initializable, ControlledScreen{
     @FXML  private TextField FieldGenero;
     @FXML  private TextField FieldEditora;
     @FXML  private TextField FieldLocation;
-    Livro selectedlivro;
-        Screens_controller myController;
-        Controller mainController = Controller.getInstance();
+    private Screens_controller myController;
+    private Controller mainController = Controller.getInstance();
 
     @Override
         public void initialize(URL url, ResourceBundle rb) {
-            selectedlivro =mainController.getSelectedBook();
-
+        Livro selectedlivro = mainController.getSelectedBook();
             FieldTitulo.setText(selectedlivro.getTitulo());
             FieldAutor.setText(selectedlivro.getAutor());
             FieldGenero.setText(selectedlivro.getGenero());
             FieldEditora.setText(selectedlivro.getEditora());
-
-
+            FieldLocation.setText(selectedlivro.getPosicao());
         }
 
         public void setScreenParent(Screens_controller screenParent) {
@@ -52,6 +39,10 @@ public class ViewEditLivros implements Initializable, ControlledScreen{
         private void goToViewLivros(ActionEvent event) {
             myController.setScreen("ViewLivros");
             myController.unloadScreen("VewEditLivros");
+        }
+        @FXML
+        private void editBook(ActionEvent event){
+            System.out.println("teste");
         }
     }
 
