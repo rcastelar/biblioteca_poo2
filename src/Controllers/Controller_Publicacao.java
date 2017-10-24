88b54d981e3a4f8ecec9bb2cde7158a9b41a2d0c
rcastelar <30738176+rcastelar@users.noreply.github.com>
2017-10-24 10:21:39 -0200

    Add files via upload

---
+++ src/Controllers/Controller_Publicacao.java
@@ -0,0 +1,81 @@
+package Controllers;
+
+import biblioteca.Exemplar;
+import biblioteca.Livro;
+import biblioteca.Livro_DAO;
+import biblioteca.Publicacao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Controller_Publicacao {
+    private static Controller_Publicacao instance;
+    private Publicacao selectedPub;
+    private Exemplar selectedExemplar;
+    private static int qdePublicacao;
+    private static ObservableList<Livro> listaLivros = FXCollections.observableArrayList();
+
+    private Controller_Publicacao() {
+        Livro_DAO myLivroDao = new Livro_DAO();
+        listaLivros = myLivroDao.getAllLivro();
+    }
+
+    public static Controller_Publicacao getInstance() {
+        if (instance == null) {
+            instance = new Controller_Publicacao();
+        }
+        return instance;
+    }
+
+    public ObservableList getListaLivros() {
+        return listaLivros;
+    }
+
+    public Publicacao getSelectedPublicacao() {
+        return selectedPub;
+    }
+
+    public void setSelectedPublicacao(Publicacao gid) {
+        selectedPub = gid;
+    }
+
+    public Exemplar getSelectedExemplar() {
+        return selectedExemplar;
+    }
+
+    public void setSelectedExemplar(Exemplar gid) {
+        selectedExemplar = gid;
+    }
+
+    public void addLivro(Livro myLivro) {
+        Livro_DAO myLivroDao = new Livro_DAO();
+        listaLivros.add(myLivro);
+        myLivroDao.InsertLivro(listaLivros);
+    }
+
+    public void editLivro(Livro myLivro) {
+        Livro_DAO myLivroDao = new Livro_DAO();
+        ((Livro) selectedPub).setAutor(myLivro.getAutor());
+        selectedPub.setEditora(myLivro.getEditora());
+        selectedPub.setGenero(myLivro.getGenero());
+        selectedPub.setPosicao(myLivro.getPosicao());
+        selectedPub.setTitulo(myLivro.getTitulo());
+        myLivroDao.InsertLivro(listaLivros);
+    }
+
+    public void addExemplar(Exemplar myExemplar) {
+        Livro_DAO myLivroDao = new Livro_DAO();
+        for (Livro neededBook : listaLivros) {
+            if (selectedPub.getId() == neededBook.get().getId()) {
+                neededBook.addToListaExempar(myExemplar);
+                break;
+            }
+        }
+        myLivroDao.InsertLivro(listaLivros);
+    }
+
+    public void removeExemplar(Exemplar myExemplar) {
+        Livro_DAO myLivroDao = new Livro_DAO();
+        selectedPub.removeExemplar(myExemplar);
+    }
+}
+
