76059a78e657649410764487346cc42e3dc68550
MarianaQuintiliano <m183711@g.unicamp.br>
2017-10-24 21:51:22 -0200

    - Pesquisa em tempo real - ViewLivros - Obrigatoriedade do campo Título na criação do Livro - Obrigatoriedade do campo Título na edição do Livro

--- src/Views/ViewNewLivro.java
+++ src/Views/ViewNewLivro.java
@@ -38,19 +38,30 @@ public class ViewNewLivro implements Initializable, ControlledScreen {
 
     @FXML
     private void newBook(ActionEvent event) {
-        int id = 1;
-        Livro mybook = new Livro(mainControllerPublicacao.getListaLivros().size() + 1, FieldTitulo.getText(), FieldLocation.getText(), FieldAutor.getText(), FieldGenero.getText(), FieldEditora.getText());
-        mainControllerPublicacao.addLivro(mybook);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("iLibrary");
-        alert.setHeaderText(null);
-        alert.setContentText("Livro salvo.");
-        FieldTitulo.setText("");
-        FieldAutor.setText("");
-        FieldLocation.setText("");
-        FieldGenero.setText("");
-        FieldEditora.setText("");
-        alert.showAndWait();
+        if(!FieldTitulo.getText().isEmpty()) {
+            int id = 1;
+            Livro mybook = new Livro(mainControllerPublicacao.getListaLivros().size() + 1, FieldTitulo.getText(), FieldLocation.getText(), FieldAutor.getText(), FieldGenero.getText(), FieldEditora.getText());
+            mainControllerPublicacao.addLivro(mybook);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("iLibrary");
+            alert.setHeaderText(null);
+            alert.setContentText("Livro salvo.");
+            FieldTitulo.setText("");
+            FieldAutor.setText("");
+            FieldLocation.setText("");
+            FieldGenero.setText("");
+            FieldEditora.setText("");
+            alert.showAndWait();
+        }
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("iLibrary");
+            alert.setHeaderText(null);
+            alert.setContentText("Livro não pode ser salvo. Preencha o campo Título!");
+            alert.showAndWait();
+            FieldTitulo.setStyle("-fx-border-style:solid; -fx-border-color: red; -fx-border-radius: 5px; -fx-effect: dropshadow(three-pass-box, red, 10, 0, 0, 0);");
+        }
 //
     }
 }
